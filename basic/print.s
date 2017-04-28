3de4b941abb03406207225bd17c57fd9c60fbb34
Andreas Reichel <webmaster@6th-dimension.com>
2017-04-28 23:29:43 +0200

    PRINT: Fix newline bug with ","
    PRINT added a superfluous new line character after every "," and
    after the end of the output.

--- basic/print.s
+++ basic/print.s
@@ -51,45 +51,9 @@ PRINT2:
         bmi     PRSTRING
         jsr     FOUT
         jsr     STRLIT
-.ifndef CONFIG_NO_CR
-        ldy     #$00
-        lda     (FAC_LAST-1),y
-        clc
-        adc     POSX
-  .ifdef KBD
-        cmp     #$28
-  .else
-        cmp     Z17
-  .endif
-        bcc     L29B1
-        jsr     CRDO
-L29B1:
-.endif
         jsr     STRPRT
-.ifdef KBD
-        jmp     L297E
-.else
         jsr     OUTSP
         bne     L297E ; branch always
-.endif
-
-.ifdef KBD
-; PATCHES
-LE86C:
-        pla
-        jmp     CONTROL_C_TYPED
-LE870:
-        jsr     GETBYT
-        txa
-LE874:
-        beq     LE878
-        bpl     LE8F2
-LE878:
-        jmp     IQERR
-; PATCHES
-.endif
-
-
 
 .ifndef KBD
 L29B9:
@@ -98,15 +62,6 @@ L29B9:
         sta     INPUTBUFFER,x
         ldx     #<(INPUTBUFFER-1)
         ldy     #>(INPUTBUFFER-1)
-  .else
-    .ifndef APPLE
-        ldy     #$00
-        sty     INPUTBUFFER,x
-        ldx     #LINNUM+1
-    .endif
-    .if .def(MICROTAN) || .def(SYM1)
-        bne     CRDO2
-	.endif
   .endif
   .ifdef CONFIG_FILE
         lda     CURDVC
@@ -132,11 +87,6 @@ CRDO2:
         jsr     OUTDO
 
 PRINTNULLS:
-.if .def(KBD) || .def(AIM65)
-        lda     #$00
-        sta     POSX
-        eor     #$FF
-.else
   .if .def(CONFIG_NULL) || .def(CONFIG_PRINTNULLS)
     .ifdef CONFIG_FILE
     ; Although there is no statement for it,
@@ -150,11 +100,7 @@ PRINTNULLS:
         pha
         ldx     Z15
         beq     L29D9
-      .ifdef SYM1
-        lda     #$FF
-      .else
         lda     #$00
-      .endif
 L29D3:
         jsr     OUTDO
         dex
@@ -164,103 +110,50 @@ L29D9:
         pla
         tax
   .else
-    .ifndef CONFIG_2
-        lda     #$00
-        sta     POSX
-    .endif
         eor     #$FF
   .endif
-.endif
 L29DD:
         rts
 L29DE:
         lda     POSX
-.ifndef CONFIG_NO_CR
-  .ifdef KBD
-        cmp     #$1A
-  .else
-        cmp     Z18
-  .endif
-        bcc     L29EA
-        jsr     CRDO
-        jmp     L2A0D
-L29EA:
-.endif
+;.ifndef CONFIG_NO_CR
+;        cmp     Z18
+;        bcc     L29EA
+;        jsr     CRDO
+;        jmp     L2A0D
+;L29EA:
+;.endif
         sec
 L29EB:
-.if .def(CONFIG_CBM_ALL) || .def(AIM65)
         sbc     #$0A
-.else
-  .ifdef KBD
-        sbc     #$0D
-  .else
-        sbc     #$0E
-  .endif
-.endif
         bcs     L29EB
         eor     #$FF
         adc     #$01
         bne     L2A08
 L29F5:
-.ifdef CONFIG_11A
         php
-.else
-        pha
-.endif
         jsr     GTBYTC
         cmp     #')'
-.ifdef CONFIG_11A
-  .ifdef CONFIG_2
         bne     SYNERR4
-  .else
-        jne     SYNERR
-  .endif
         plp
         bcc     L2A09
-.else
-  .ifdef CONFIG_11
-        jne     SYNERR
-  .else
-        bne     SYNERR4
-  .endif
-        pla
-        cmp     #TOKEN_TAB
-  .ifdef CONFIG_11
-        bne     L2A09
-  .else
-        bne     L2A0A
-  .endif
-.endif
         txa
         sbc     POSX
         bcc     L2A0D
-.ifndef CONFIG_11
         beq     L2A0D
-.endif
 L2A08:
         tax
-.ifdef CONFIG_11
 L2A09:
         inx
-.endif
 L2A0A:
-.ifndef CONFIG_11
-        jsr     OUTSP
-.endif
         dex
-.ifndef CONFIG_11
-        bne     L2A0A
-.else
         bne     L2A13
-.endif
 L2A0D:
         jsr     CHRGET
         jmp     PRINT2
-.ifdef CONFIG_11
 L2A13:
         jsr     OUTSP
         bne     L2A0A
-.endif
 
 ; ----------------------------------------------------------------------------
 ; PRINT STRING AT (Y,A)
@@ -288,8 +181,6 @@ L2A22:
         jmp     L2A22
 ; ----------------------------------------------------------------------------
 OUTSP:
-.ifdef CONFIG_FILE
-  .ifndef CBM1
 ; on non-screen devices, print SPACE
 ; instead of CRSR RIGHT
         lda     CURDVC
@@ -297,11 +188,8 @@ OUTSP:
         lda     #$20
         .byte   $2C
 LCA40:
-  .endif
-        lda     #$1D ; CRSR RIGHT
-.else
-        lda     #$20
-.endif
+;        lda     #$1D ; CRSR RIGHT
+        lda #$20
         .byte   $2C
 OUTQUES:
         lda     #$3F
@@ -310,82 +198,14 @@ OUTQUES:
 ; PRINT CHAR FROM (A)
 ; ----------------------------------------------------------------------------
 OUTDO:
-.ifndef KBD
         bit     Z14
         bmi     L2A56
-.endif
-.if .def(CONFIG_PRINT_CR) || .def(CBM1)
-; Commodore forgot to remove this in CBM1
-        pha
-.endif
-.ifdef CBM1
-        cmp     #$1D ; CRSR RIGHT
-        beq     LCA6A
-        cmp     #$9D ; CRSR LEFT
-        beq     LCA5A
-        cmp     #$14 ; DEL
-        bne     LCA64
-LCA5A:
-        lda     POSX
-        beq     L2A4E
-        lda     CURDVC
-        bne     L2A4E
-        dec     POSX
-LCA64:
-        and     #$7F
-.endif
-.ifndef CBM2
-        cmp     #$20
-        bcc     L2A4E
-.endif
-LCA6A:
-.ifdef CONFIG_CBM1_PATCHES
-        lda     CURDVC
-        jsr     PATCH6
-        nop
-.endif
-.ifdef CONFIG_PRINT_CR
-        lda     POSX
-        cmp     Z17
-        bne     L2A4C
-  .ifdef AIM65
-        lda #$00
-        sta POSX
-  .elseif .def(APPLE)
-        nop ; PATCH!
-        nop ; don't print CR
-        nop
-  .else
-        jsr     CRDO
-  .endif
-L2A4C:
-.endif
-.ifndef CONFIG_CBM_ALL
-        inc     POSX
-.endif
-L2A4E:
-.if .def(CONFIG_PRINT_CR) || .def(CBM1)
-; Commodore forgot to remove this in CBM1
-        pla
-.endif
 .ifdef CONFIG_MONCOUT_DESTROYS_Y
         sty     DIMFLG
 .endif
-.ifdef CONFIG_IO_MSB
-        ora     #$80
-.endif
         jsr     MONCOUT
-.ifdef CONFIG_IO_MSB
-        and     #$7F
-.endif
 .ifdef CONFIG_MONCOUT_DESTROYS_Y
         ldy     DIMFLG
-.endif
-.ifdef OSI
-        nop
-        nop
-        nop
-        nop
 .endif
 L2A56:
         and     #$FF
