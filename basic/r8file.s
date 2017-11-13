f167f5cc7b5991530b226ba7c90b6c907fed3002
Andreas J. Reichel <andreas.reichel@tngtech.com>
2017-11-13 13:02:20 +0100

    WIP

---
+++ basic/r8file.s
@@ -0,0 +1,20 @@
+.segment "BASCODE"
+
+
+CHKIN:  nop
+        rts
+
+CHCK:   nop
+        rts
+
+CLOSE:  nop
+        rts
+
+OPEN:   nop
+        rts
+
+CHKOUT:  nop
+        rts
+
+CLRCH:  nop
+        rts
