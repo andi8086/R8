930e37da7507dd391ad0abd5ffad46a3aaf85656
Andreas Reichel <webmaster@6th-dimension.com>
2018-01-02 22:27:22 +0100

    Add Character Generation Function
    Add needed multiplication function
    Add offset calculation function
    Add character render function

--- main.s
+++ main.s
@@ -107,6 +107,39 @@ serial_getc_echo:   JSR serial_getc
 
 .export kernal_load_file
 
+    ;----------------------------------------------
+    ; KERNAL INT MATHS: erg16 += x16 * y8
+    ;
+    ; (KM_ERG,KM_ERGHI) += (KM_X,KM_XHI) * KM_Y
+    ; by "white flame", modified by me (last 7 lines)
+    ;----------------------------------------------
+kernal_multiply:
+                    LDA #$00
+                    TAY
+                    BEQ km_enterLoop
+km_doAdd:
+                    CLC
+                    ADC KM_X
+                    TAX
+                    TYA
+                    ADC KM_XHI
+                    TAY
+                    TXA
+km_loop:
+                    ASL KM_X
+                    ROL KM_XHI
+km_enterLoop:  ; accumulating multiply entry point (enter with .A=lo, .Y=hi)
+                    LSR KM_Y
+                    BCS km_doAdd
+                    BNE km_loop
+                    CLC
+                    ADC KM_ERG
+                    STA KM_ERG
+                    TYA
+                    ADC KM_ERGHI
+                    STA KM_ERGHI
+                    RTS
+
 printn:
                     LDA #0
                     TAY
@@ -175,7 +208,7 @@ rga_waitsync:
                     BPL rga_waitsync
 rga_waitnolock:     BIT SYSCTRL_A
                     BMI rga_waitnolock
-		    RTS
+                    RTS
 
     ;----------------------------------------------
     ; KERNAL RGA clear screen
@@ -185,7 +218,7 @@ rga_waitnolock:     BIT SYSCTRL_A
 rga_clrscr:
                     LDX #3              ; start with video bank 3
                     STX RGA_CURBANK
-                    LDA RGA_BKCOLOR
+                    LDA RGA_BGCOLOR
 rga_clrscr_nextpage:
                     LDX RGA_CURBANK
                     STX SYSCTRL_A       ; set video bank
@@ -213,6 +246,171 @@ rga_clrscr_loop:
                     BPL rga_clrscr_nextpage ; and continue if bank >= 0
                     RTS
     
+    ;----------------------------------------------
+    ; KERNAL RGA calculate ram offset
+    ;
+    ; Input: (RGA_VIDEOCOL) = column, (RGA_VIDEOROW) = row
+    ; Output: (RGA_VIDPOINTER) = offset in bank (starting at $A000)
+    ;         (RGA_CURBANK) = bank
+    ;----------------------------------------------
+rga_calc_ramoffset:
+                    LDA RGA_VIDEOROW
+                    STA KM_Y
+                    LDA #$90
+                    STA KM_X
+                    LDA #$6
+                    STA KM_XHI ; one character line has $690 bytes (210*8)
+                    LDA #0
+                    STA KM_ERG
+                    STA KM_ERGHI
+                    JSR kernal_multiply
+                    LDA RGA_VIDEOCOL
+                    STA KM_X
+                    LDA #0
+                    STA KM_XHI
+                    STA RGA_CURBANK
+                    LDA #6
+                    STA KM_Y
+                    JSR kernal_multiply ; erg = row * 1680 + col * 6
+                    LDA KM_ERG
+                    STA RGA_VIDPOINTER
+                    LDA KM_ERGHI
+rga_co_checkbank:
+                    CMP #$20
+                    BCS rga_co_incbank
+                    ADC #$A0
+                    STA RGA_VIDPOINTER+1
+                    LDA RGA_CURBANK
+                    STA SYSCTRL_A
+                    RTS
+rga_co_incbank:
+                    SBC #$20
+                    INC RGA_CURBANK
+                    BNE rga_co_checkbank ; always true
+
+    ;----------------------------------------------
+    ; KERNAL RGA render char at current vidpointer
+    ;
+    ; Input: (RGA_VIDPOINTER) = offset in bank
+    ;        (RGA_CURBANK) = bank
+    ;        (RGA_FGCOLOR) = foreground color
+    ;        (RGA_BGCOLOR) = background color
+    ;        A = character code
+    ;----------------------------------------------
+
+rga_renderchar:
+                    CLD
+                    STA KM_X
+                    LDX #$0
+                    STX KM_XHI ; KM_X = ASCII extended to 16 bits
+                    STX KM_ERG ; low byte of result to 0
+                    LDX #$CA   ; store character ROM page into high byte
+                    STX KM_ERGHI
+                    CLC
+                    ASL KM_X    ; multiply KM_X by 2
+                    ROL KM_XHI
+                    LDA KM_X
+                    STA KM_ERG  ; and add it to KM_ERG. Use STA since ERG_LO is 0
+                    LDA KM_XHI
+                                ; carry should be clear here
+                    ADC KM_ERGHI
+                    STA KM_ERGHI
+                    ASL KM_X    ; multiply KM_X by 2 again
+                    ROL KM_XHI
+                                ; carry should be clear here
+                    LDA KM_X
+                    ADC KM_ERG  ; add KM_X to KM_ERG
+                    STA KM_ERG
+                    LDA KM_XHI
+                    ADC KM_ERGHI
+                    STA KM_ERGHI
+                                ; (KM_ERG) = $CA00 + A * 6;
+                    LDA #$80
+                    STA ZP_TMP  ; init bitmask
+                    LDA #0      ; Initialize Y-Counter
+                    JSR rga_waitsync
+loopy:
+                      PHA       ; save Y-Counter
+                      LDY #0    ; init index into bitmask, which is the pixel-column
+loopx:
+                        LDA (KM_ERG),Y  ; load character pattern
+                        BIT ZP_TMP      ; test pattern against bitmask
+                        PHP         ; save result flags
+                        LDA RGA_FGCOLOR
+                        PLP         ; restore result flags
+                        BNE drawfg  ; skip if bit was set
+                        LDA RGA_BGCOLOR ; draw background instead
+drawfg:
+                        LDX #0
+                        STA (RGA_VIDPOINTER,X) ; store to VRAM
+                        LDX #$BF
+                        INC RGA_VIDPOINTER   ; increase VRAM offset
+                        BNE incvoffs1 ; if overflow, increase high byte
+                        INC RGA_VIDPOINTER+1
+incvoffs1:
+                        CPX RGA_VIDPOINTER+1 ; check for bank overrun
+                        BCS stay_in_bank ; if X >= (VOFFS+1), no bank overrun and
+                                         ; carry set. Else carry clear.
+                        LDA RGA_VIDPOINTER+1
+                        SBC #$21   ; A = A - M - (C - 1). Since carry is cleared
+                                   ; this subtracts $20
+                        STA RGA_VIDPOINTER+1
+                        INC RGA_CURBANK
+                        LDA RGA_CURBANK
+                        STA $8001
+stay_in_bank:
+                        INY
+                        CPY #6
+                        BMI loopx ; loop if Y < 6
+                      LSR ZP_TMP  ; C should be clear afterwards
+                      LDA RGA_VIDPOINTER
+                      ADC #204
+                      STA RGA_VIDPOINTER
+                      LDA RGA_VIDPOINTER+1
+                      ADC #0
+                      CMP #$BF  ;if A >= $BF, C flag is set
+                      BCC stay_in_bank2
+                        ; carry is set here. This works the other way arround than
+                        ; the compare above. Here the limit is the operand, above
+                        ; the value is the operand.
+                      SBC #$20
+                      INC RGA_CURBANK
+                      LDY RGA_CURBANK
+                      STY $8001
+stay_in_bank2:
+                      STA RGA_VIDPOINTER+1 ; store modified high byte for addition
+                             ; and correction
+                      PLA ; restore Y-Counter
+                      TAY
+                      INY ; increase Y-Counter and transfer to A
+                      TYA
+                      CPY #8 ; Continue if Y-Counter < 8
+                      BMI loopy
+
+                    RTS
+
+kernal_test_chars:
+                    LDA #255
+                    LDX #18
+                    STX RGA_VIDEOROW
+row_loop:
+                    LDX #34
+                    STX RGA_VIDEOCOL
+col_loop:
+                    PHA
+                    JSR rga_calc_ramoffset
+                    PLA
+                    PHA
+                    JSR rga_renderchar
+                    PLA
+                    SEC
+                    SBC #1
+                    DEC RGA_VIDEOCOL
+                    BPL col_loop
+                    DEC RGA_VIDEOROW
+                    BPL row_loop
+                    RTS
+
 __nmi:
     NOP
     RTI
